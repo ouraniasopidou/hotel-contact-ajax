f649d5ba90e660db80b1aab9930d911eacee711f
ouraniasopidou <rania_sg@hotmail.com>
2025-03-28 20:51:00 +0200

    Initial commit - hotel contact AJAX

---
+++ hotels.php
@@ -0,0 +1,20 @@
+<?php
+
+if($_GET["select"] == "1") {
+    echo 'Grand Hotel London<br>';
+    echo 'Address: 123 Example Street, London, UK<br>';
+    echo 'Contact Number: +44 1234 567890<br>';
+}
+
+if($_GET["select"] == "2") {
+    echo 'Grand Hotel Athens<br>';
+    echo 'Address: 456 Demo Avenue, Athens, Greece<br>';
+    echo 'Contact Number: +30 210 0000000<br>';
+}
+
+if($_GET["select"] == "3") {
+    echo 'Grand Hotel Paris<br>';
+    echo 'Address: 789 Test Road, Paris, France<br>';
+    echo 'Contact Number: +33 1 23 45 67 89<br>';
+}
+?>
